39bfc882d8a76f46821c5026aa1b92d8a7c30307
Anousone Kaseumsouk <anousonekaseumsouk@gmail.com>
2018-07-30 09:56:08 -0700

    Second commit, using linter.

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,13 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: "eslint-loader"
